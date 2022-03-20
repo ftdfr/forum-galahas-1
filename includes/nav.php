b8ea10284f9527f21208587c3feb988313f19b38
Aftiweb <77934332+Aftiweb@users.noreply.github.com>
2022-03-20 12:36:19 +0100

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -7,9 +7,10 @@
     <img src="Pictures/logo.png" alt="Logo Galahas's Forum" class="logo">
     <nav>
         <ul class="nav__links">
-            <li><a href="">News</a></li>
-            <li><a href="">Sujet</a></li>
-            <li><a href="">Contact</a></li>
+            <li><a href="">Actus</a></li>
+            <li><a href="">Acceuil</a></li>
+            <li><a href="">Favoris</a></li>
+            <li><a href="">Messages privés</a></li>
         </ul>
     </nav>
     <a><button id="connect-button">Connexion</button></a>
